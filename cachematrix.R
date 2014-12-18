399ad5c8fccac2f9aa8996dbb9382de78f75acbd
sal1944 <salomea.swierkowska@gmail.com>
2014-12-18 15:36:59 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These 2 functions are set designed to store and return values of 
+## inputed matrix (must be inverasble), and its inverse
+## I.e. z<-matrix(1:4,2,2) we create some matrix
+## a<-makeCacheMatrix(z) - creates object
+## (a) with matrix (z) saved inside
+## cacheSolve(a) - used 1st time counts and store matrix inverted to matrix (z)
+## within (a) object. Next time; if we won't change oryginal matrix saved inside 
+## (a) object function will just call its inverse from cache memory associated with (a) object 
 
-## Write a short comment describing this function
-
+## makeCacheMatrix function creates matrix-based object 
+## (list of functions on matricies explicite) able to store
+## given matrix and its inverse matrix 'within' the object.
+## Use cacheSolve to count, store and call inversed matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+	m<-NULL
+	set<-function(y){
+		x<<-y
+		m<<-NULL
+	}
+	get<-function()x
+	setSolve<-function(m_inverse)m<<-m_inverse
+	getSolve<-function()m
+	list(get=get,set=set,getSolve=getSolve,setSolve=setSolve)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve returns an inverse matrix from cache 
+## (from variables set within makeCacheMatrix function), 
+## or if imposible, solve it, so this function alows us to skip 
+## evaluating process if it has been done alredy
+## works only on makeCacheMatrix object
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m=x$getSolve()
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setSolve(m)
+	m
 }
